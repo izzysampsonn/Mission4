a4c1e6ada17c9740ae1e45c857c55c52ae523156
Izzy Sampson <isabellesampson3@gmail.com>
2023-02-06 10:39:50 -0700

    Finished the videos, new page for form

---
+++ Mission4/Controlers/firstViewController.cs
@@ -0,0 +1,25 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace Mission4.Controlers
+{
+    public class firstViewController : Controller
+    {
+        public IActionResult Index()
+        {
+
+            // throw an error
+            //throw new IndexOutOfRangeException();
+
+            return View();
+        }
+
+        public IActionResult Form()
+        {
+            return View();
+        }
+    }
+}
